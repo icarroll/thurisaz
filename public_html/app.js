6c121ec3917d61585cf51654fdee951cd81acbb6
Isaac Carroll <icarroll@pdx.edu>
2019-01-09 10:18:58 -0800

    full hotseat play works

--- public_html/app.js
+++ public_html/app.js
@@ -23,6 +23,8 @@ function coord(x,y) {
 var app = new Vue({
   data: {
     COLS: ['A','B','C','D','E','F','G','H','J','K','L','M','N','O','P'],
+    dwarfscapt: 0,
+    trollscapt: 0,
     isdwarfturn: true,
     thudboard: [
         ['#','#','#','#','#','d','d',' ','d','d','#','#','#','#','#'],
@@ -42,10 +44,13 @@ var app = new Vue({
         ['#','#','#','#','#','d','d',' ','d','d','#','#','#','#','#']],
     legalmoveto: Array(15).fill(0).map(x=>Array(15).fill(false)),
     legalattack: Array(15).fill(0).map(x=>Array(15).fill(false)),
+    legalshove: Array(15).fill(0).map(x=>Array(15).fill(false)),
+    legaltarget: Array(15).fill(0).map(x=>Array(15).fill(false)),
     selectpiece: true,
     selectdest: false,
     selectattack: false,
     movefrom: {x:-1,y:-1},
+    attackfrom: {x:-1,y:-1},
   },
   methods: {
     selectable: function (x,y) {
@@ -54,17 +59,21 @@ var app = new Vue({
                 || ! this.isdwarfturn && this.thudboard[y][x] == 'T');
       }
       else if (this.selectdest) return this.legalmoveto[y][x];
+      else if (this.selectattack) return this.legaltarget[y][x];
       else return false;
     },
     selected: function (x,y) {
       if (this.selectpiece) return false;
       else if (this.selectdest) return this.movefrom.x == x
                                        && this.movefrom.y == y;
+      else if (this.selectattack) return this.attackfrom.x == x
+                                         && this.attackfrom.y == y;
       else return false;
     },
     isattack: function (x,y) {
       if (this.selectpiece) return false;
       else if (this.selectdest) return this.legalattack[y][x];
+      else if (this.selectattack) return this.legaltarget[y][x];
       else return false;
     },
     click: function (x,y) {
@@ -81,24 +90,68 @@ var app = new Vue({
 
           return;
         }
+        // else fall through to clear state
       }
       else if (this.selectdest) {
         if (this.legalmoveto[y][x]) {
           var piece = this.thudboard[this.movefrom.y][this.movefrom.x];
           Vue.set(this.thudboard[this.movefrom.y], this.movefrom.x, ' ');
           Vue.set(this.thudboard[y], x, piece);
+          if (this.legalattack[y][x]) {
+            if (this.isdwarfturn) {
+              this.trollscapt += 1;
+            }
+            else {
+              var attacks = this.alldwarfneighbors(coord(x,y));
+              if (this.legalshove[y][x] || attacks.length == 1) {
+                for (var ix=0 ; ix<attacks.length ; ix+=1) {
+                  Vue.set(this.thudboard[attacks[ix].y], attacks[ix].x, ' ');
+                  this.dwarfscapt += 1;
+                }
+              }
+              else {
+                // troll attack which isn't shove with multiple dwarfs adjacent
+                this.selectpiece = false;
+                this.selectdest = false;
+                this.selectattack = true;
+                this.attackfrom = {x:x,y:y};
+                for (var ix=0 ; ix<attacks.length ; ix+=1) {
+                  Vue.set(this.legaltarget[attacks[ix].y], attacks[ix].x, true);
+                }
+                return;
+              }
+            }
+          }
           this.isdwarfturn = ! this.isdwarfturn;
           // fall through to clear state
         }
+        // else fall through to clear state
+      }
+      else if (this.selectattack) {
+        if (this.legaltarget[y][x]) {
+          Vue.set(this.thudboard[y], x, ' ');
+          this.dwarfscapt += 1;
+          this.isdwarfturn = ! this.isdwarfturn;
+        }
+        else {
+          // reset troll partial move
+          var piece = this.thudboard[this.attackfrom.y][this.attackfrom.x];
+          Vue.set(this.thudboard[this.attackfrom.y], this.attackfrom.x, ' ');
+          Vue.set(this.thudboard[this.movefrom.y], this.movefrom.x, piece);
+        }
+        //fall through to clear state
       }
 
-      // cancel move: clear state
+      // move canceled or completed: clear state
       this.movefrom = {x:-1,y:-1};
+      this.attackfrom = {x:-1,y:-1};
       this.selectpiece = true;
       this.selectdest = false;
       this.selectattack = false;
       this.legalmoveto = Array(15).fill(0).map(x=>Array(15).fill(false));
       this.legalattack = Array(15).fill(0).map(x=>Array(15).fill(false));
+      this.legalshove = Array(15).fill(0).map(x=>Array(15).fill(false));
+      this.legaltarget = Array(15).fill(0).map(x=>Array(15).fill(false));
     },
     setlegalmoveto : function() {
       if (this.isdwarfturn) {
@@ -106,7 +159,6 @@ var app = new Vue({
           for (var dist=1 ; dist<SIZE ; dist+=1) {
             var dest = coord(this.movefrom.x + dirs[ix].dx*dist,
                              this.movefrom.y + dirs[ix].dy*dist);
-            console.log(dest);
             if (inbounds(dest)) {
               if (this.thudboard[dest.y][dest.x] == ' ') {
                 Vue.set(this.legalmoveto[dest.y], dest.x, true);
@@ -130,6 +182,73 @@ var app = new Vue({
       }
     },
     setlegalattack : function() {
+      if (this.isdwarfturn) {
+        for (var ix=0 ; ix<dirs.length ; ix+=1) {
+          for (var dist=1 ; dist<SIZE ; dist+=1) {
+            var dest = coord(this.movefrom.x + dirs[ix].dx*dist,
+                             this.movefrom.y + dirs[ix].dy*dist);
+            var check = coord(this.movefrom.x - dirs[ix].dx*(dist-1),
+                              this.movefrom.y - dirs[ix].dy*(dist-1));
+            if (! inbounds(check)) break;
+            if (this.thudboard[check.y][check.x] != 'd') break;
+            if (! inbounds(dest)) break;
+            if (this.thudboard[dest.y][dest.x] == 'T') {
+              Vue.set(this.legalmoveto[dest.y], dest.x, true);
+              Vue.set(this.legalattack[dest.y], dest.x, true);
+              break;
+            }
+            if (this.thudboard[dest.y][dest.x] != ' ') break;
+          }
+        }
+      }
+      else {
+        for (var ix=0 ; ix<dirs.length ; ix+=1) {
+          for (var dist=1 ; dist<SIZE ; dist+=1) {
+            var dest = coord(this.movefrom.x + dirs[ix].dx*dist,
+                             this.movefrom.y + dirs[ix].dy*dist);
+            var check = coord(this.movefrom.x - dirs[ix].dx*(dist-1),
+                              this.movefrom.y - dirs[ix].dy*(dist-1));
+            var check2 = coord(this.movefrom.x - dirs[ix].dx*dist,
+                               this.movefrom.y - dirs[ix].dy*dist);
+            if (! inbounds(check)) break;
+            if (this.thudboard[check.y][check.x] != 'T') break;
+            if (! inbounds(dest)) break;
+            if (this.thudboard[dest.y][dest.x] != ' ') break;
+            if (this.hasdwarfneighbor(dest)) {
+              Vue.set(this.legalmoveto[dest.y], dest.x, true);
+              Vue.set(this.legalattack[dest.y], dest.x, true);
+              if (dist > 1 || inbounds(check2)
+                              && this.thudboard[check2.y][check2.x] == 'T') {
+                Vue.set(this.legalshove[dest.y], dest.x, true);
+              }
+            }
+          }
+        }
+      }
+    },
+    hasdwarfneighbor: function (pos) {
+      for (var ix=0 ; ix<dirs.length ; ix+=1) {
+        var check = coord(pos.x + dirs[ix].dx, pos.y + dirs[ix].dy);
+        if (inbounds(check) && this.thudboard[check.y][check.x] == 'd') {
+          return true;
+        }
+      }
+      return false;
+    },
+    alldwarfneighbors: function (pos) {
+      var neighbors = [];
+      for (var ix=0 ; ix<dirs.length ; ix+=1) {
+        var check = coord(pos.x + dirs[ix].dx, pos.y + dirs[ix].dy);
+        if (inbounds(check) && this.thudboard[check.y][check.x] == 'd') {
+          neighbors.push(check);
+        }
+      }
+
+      return neighbors;
+    },
+    pl: function(n) {
+      if (n == 1) return '';
+      else return 's';
     },
   },
 });
